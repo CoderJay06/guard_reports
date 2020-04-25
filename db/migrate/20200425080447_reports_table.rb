33a8b29aa025b89e8c3d3fe297f6210ed6929205
CoderJay06 <jaycru06@yahoo.com>
2020-04-25 04:40:44 -0400

    Add columns to reports table

--- db/migrate/20200425080447_reports_table.rb
+++ db/migrate/20200425080447_reports_table.rb
@@ -1,4 +1,11 @@
 class ReportsTable < ActiveRecord::Migration
   def change
+    create_table :reports do |t|
+      t.string :type 
+      t.date :date
+      t.time :time
+      t.string :location
+      t.string :description
+    end 
   end
 end
