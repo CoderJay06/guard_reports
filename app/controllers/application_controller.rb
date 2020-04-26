6568e69d260edac44858f82e09f988469a5dc791
Jason J Cruz <jaycru06@yahoo.com>
2020-04-26 08:34:09 +0000

    Add comments, build out login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,8 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "101fce11d312b2b21fa969badda2db9aa290b08b3cf1a80b5b586876d21d39a1364291a83594a49429f123ed7119648d5732d282ee2
     98182c10b5ec5b1de3042"
   end
-
+  
+  # renders the index (homepage)
   get '/' do
     erb :index
   end
@@ -18,11 +19,12 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!session[:id]
     end 
-
+    
     def current_user 
       Guard.find_by(session[:id])
     end 
-
+    
+    # determine if user signup fields are filled out
     def fields_empty? 
       params[:name] == " " || params[:email] == " " || params[:company] == " " || 
       params[:password] == " " || params[:number] == " "
--- app/controllers/guards_controller.rb
+++ app/controllers/guards_controller.rb
@@ -1,13 +1,15 @@
 class GuardsController < ApplicationController 
 
    get '/signup' do 
-
+      # render signup form page 
       erb :'guards/signup'
    end
    
    post '/signup' do 
       #binding.pry
       
+      # when all signup fields are filled out 
+      # creates a new guard/user and redirects login  
       if !fields_empty?
          @guard = Guard.create(
             name: params[:name],
@@ -16,12 +18,17 @@ class GuardsController < ApplicationController
             password: params[:password],
             company: params[:company]
          )
+         #raise params.inspect 
          redirect to "/login"
+      else 
+        # redirects back to signup if fields not filled
+        redirect "/signup"
       end 
    end 
 
    get '/login' do 
-
+     
+    erb :'guards/login'
    end 
 
    get '/logout' do 
