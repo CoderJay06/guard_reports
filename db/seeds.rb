d69e852197d52967bf88810be2b270228277de07
CoderJay06 <jaycru06@yahoo.com>
2020-04-26 00:11:13 -0400

    Added seed data and ran rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
 # seed data file 
+@guard1 = Guard.create(
+   name: "Joe Smith",
+   email: "joe@example.com",
+   number: 1234445656,
+   password: "joe1234",
+   company: "Guard Tower Security"
+)
+
+@report1 = Report.create(
+   report_type: "Daily Activity Report",
+   guard_id: @guard1.id,
+   location: "Long Island NY",
+   description: "Patroled the perimeter"
+)
