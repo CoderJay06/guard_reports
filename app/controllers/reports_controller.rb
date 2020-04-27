17e49e22b24ab7850c08e5b70bbec1c5ac9510f4
CoderJay06 <jaycru06@yahoo.com>
2020-04-27 04:42:13 -0400

    Add edit form action, report id route and patch route

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController 
-
+   # render reports page to view all reports when logged in
    get '/reports' do 
       @reports = Report.all 
       if logged_in?
@@ -8,13 +8,13 @@ class ReportsController < ApplicationController
          redirect to "/signup"
       end 
    end 
-
+   # render page to create a new report 
    get '/reports/new' do 
       @reports = Report.all 
 
       erb :'reports/new'
    end 
-
+   # creates new reports when all form fields are filled
    post '/reports' do 
       #raise params.inspect
       if report_fields_empty?
@@ -28,11 +28,25 @@ class ReportsController < ApplicationController
             location: params[:location],
             description: params[:description]
          )
+         # assign guard/user to their report
          @report.guard_id = session[:guard_id]
-         
+         # render report show page 
          erb :"reports/show_report"
       end 
    end 
-  
 
+   get '/reports/:id' do 
+      @report = Report.find_by_id(params[:id])
+      if logged_in? 
+         erb :"reports/show_report"
+      else 
+         redirect to "/login"
+      end 
+   end 
+
+   patch '/reports/:id/edit' do 
+      binding.pry
+      @report = Report.find_by_id(params[:id])
+   end 
+  
 end 
