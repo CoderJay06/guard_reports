b6e5c76c67dc14c36d3241ef18cf02d22d097499
CoderJay06 <jaycru06@yahoo.com>
2021-06-28 02:22:37 -0400

    Move session secret to env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     set :method_override, true
     enable :sessions
-    set :session_secret, "101fce11d312b2b21fa969badda2db9aa290b08b3cf1a80b5b586876d21d39a1364291a83594a49429f123ed7119648d5732d282ee2
-    98182c10b5ec5b1de3042"
+    set :session_secret, ENV["SESSION_SECRET"]
   end
   
   # renders the index (homepage)
